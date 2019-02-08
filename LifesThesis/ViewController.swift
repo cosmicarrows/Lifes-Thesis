0689edc5e6ee00eb14fbdcd51a92329c05af2120
LaurenceW <laurence@cosmicarrows.com>
2019-02-08 00:53:11 -0700

    Initial Commit

---
+++ LifesThesis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifesThesis
+//
+//  Created by Laurence Wingo on 2/8/19.
+//  Copyright © 2019 Cosmic Arrows, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
